a700e56e00c6abd4f54adfc64a73eb009a49403e
Suryansh9331 <suryanshmishra9331@gmail.com>
2025-11-03 23:07:56 +0530

    Initialization steps

--- App.jsx
+++ App.jsx
@@ -1,47 +1,7 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-import { Text } from 'react-native';
+import { Text ,SafeAreaView} from "react-native"
+const App=()=>{
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  // return (
-  //   <SafeAreaProvider>
-  //     <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-  //     <AppContent />
-  //   </SafeAreaProvider>
-  // );
-  return <Text>hello </Text>
 }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
+export default App
